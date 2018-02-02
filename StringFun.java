66aaf610a6f86cd148915bad120b38f77821e304
djbarajas <djbarajas@dons.usfca.edu>
2018-02-02 18:12:13 +0000

    All methods work

--- StringFun.java
+++ StringFun.java
@@ -12,29 +12,24 @@ public class StringFun {
 		for(int i = 0; i < args.length; i++){
 	        System.out.println("\nLength of String: " + args[i].length());
 	        System.out.println("Does it equal Hello? " + args[i].equals(aString));
-	        //System.out.println("Finding Substrings: " + args[i].substring(1));
+	        // this try catch statement was for working with null strings
+	        try{
+	        	System.out.println("Finding Substrings: " + args[i].substring(1));
+	        } catch (StringIndexOutOfBoundsException e) {
+	        	System.out.println("This string has one or less character!");
+	        }
 	        System.out.println("Trimming whitespace: " + args[i].trim());
-	        System.out.println("Finding the FIRST index of letter a: " + args[i].indexOf("a"));
-	        System.out.println("Finding the LAST index of letter a: " + args[i].lastIndexOf("a"));
-	        // this try catch statement was for when I tested a null string, an error was thrown
+	        System.out.println("Finding the FIRST index of letter a (else returns -1): " + args[i].indexOf("a"));
+	        System.out.println("Finding the LAST index of letter a (else returns -1): " + args[i].lastIndexOf("a"));
+	        // this try catch statement was for working with null strings too
 	        try{
 	        	System.out.println("Character at index 0: " + args[i].charAt(0));
 	    	} catch (StringIndexOutOfBoundsException e){
 	    		System.out.println("This string has no characters!");
 	    	}
+	    	//Two additional string methods below
 	        System.out.println("Is the string empty: " + args[i].isEmpty());
 	        System.out.println("Replacing all instances of t with +: " + args[i].replaceAll("t","+"));
      	}
 	}
 }
-/*
-s.length() 
-s.equals(otherString)
-s.substring(startIndex, endIndex) 
-s.trim() 
-s.indexOf(subString)  
-s.lastIndexOf(subString)
-s.charAt(index) 
-s.isEmpty()
-s.replaceAll(String regex, String replacement)
-*/
